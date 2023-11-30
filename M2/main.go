947d317ffce413093f83fd18bf5de09677313bda
Dimas Febriyanto <dimassfeb@gmail.com>
2023-11-30 08:52:58 +0700

    M2 updated

--- M2/main.go
+++ M2/main.go
@@ -9,6 +9,7 @@ type Mahasiswa struct {
 }
 
 func main() {
+
 	var jumlahMahasiswa int
 	fmt.Print("Masukkan jumlah mahasiswa: ")
 	fmt.Scan(&jumlahMahasiswa)
@@ -18,19 +19,21 @@ func main() {
 	for i := 0; i < len(mahasiswas); i++ {
 		fmt.Printf("Data Mahasiswa %d:\n", i+1)
 		fmt.Print("Nama: ")
-		fmt.Scan(&mahasiswas[i].Jurusan)
-		fmt.Println("- -")
 		fmt.Scan(&mahasiswas[i].Nama)
 		fmt.Print("NIM: ")
 		fmt.Scan(&mahasiswas[i].NIM)
 		fmt.Print("Jurusan: ")
+		fmt.Scan(&mahasiswas[i].Jurusan)
+		fmt.Println()
 	}
 
+	fmt.Println("\nData Mahasiswa:")
 	for i := 0; i < len(mahasiswas); i++ {
 		mahasiswaPtr := &mahasiswas[i]
+		fmt.Print(&mahasiswaPtr)
 		fmt.Printf("Nama: %s\n", mahasiswaPtr.Nama)
 		fmt.Printf("NIM: %s\n", mahasiswaPtr.NIM)
 		fmt.Printf("Jurusan: %s\n", mahasiswaPtr.Jurusan)
-		fmt.Println("-- ")
+		fmt.Println("-")
 	}
 }
